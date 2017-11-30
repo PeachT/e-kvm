64b4f6455b2509a123889ac6e05f1b8a18ca3911
892350329@qq.com <892350329@qq.com>
2017-11-30 14:24:50 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: 'airbnb-base',
+  globals: {
+    __static: true
+  },
+  plugins: [
+    'html'
+  ],
+  'rules': {
+    'global-require': 0,
+    'import/no-unresolved': 0,
+    'no-param-reassign': 0,
+    'no-shadow': 0,
+    'import/extensions': 0,
+    'import/newline-after-import': 0,
+    'no-multi-assign': 0,
+    // 禁止出现未使用过的变量
+    "no-unused-vars": [1, { "vars": "all", "args": "none" }],
+    // 允许使用devDependencies中的包
+    "import/no-extraneous-dependencies": ["error", {"devDependencies": true}],
+    "no-empty": 1,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
